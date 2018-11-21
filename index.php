3f9dac12a2c08ccc8a9fb55d193b58618285dc39
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-21 09:31:36 +0100

    exercice_php_P3_7

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP</title>
+</head>
+<body>
+  <?php
+  $max = 1;
+  while ($max <100){
+    echo ' ' . 'On tient le bon bout.';
+   $max+= 15;
+  }
+ ?>
+</body>
+</html>
